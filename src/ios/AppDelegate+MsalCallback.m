f2601e899c380c62b9180c9928a9928e246d17f1
Marcel Staber <m.staber@uniqbit.de>
2021-01-08 10:25:46 +0100

    added special handling for fiori client

--- src/ios/AppDelegate+MsalCallback.m
+++ src/ios/AppDelegate+MsalCallback.m
@@ -14,8 +14,35 @@
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
 {
-    return [MSALPublicClientApplication handleMSALResponse:url
-                                         sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]];
+
+
+
+    if (!url) {
+        return NO;
+    }
+
+    if ([url.absoluteString rangeOfString:@"x-callback-url"].location == NSNotFound) {
+        return [MSALPublicClientApplication handleMSALResponse:url
+                                            sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]];
+    } 
+
+    NSMutableDictionary * openURLData = [[NSMutableDictionary alloc] init];
+
+    [openURLData setValue:url forKey:@"url"];
+
+    if (options[UIApplicationOpenURLOptionsSourceApplicationKey]) {
+        [openURLData setValue:options[UIApplicationOpenURLOptionsSourceApplicationKey] forKey:@"sourceApplication"];
+    }
+
+    if (options[UIApplicationOpenURLOptionsAnnotationKey]) {
+        [openURLData setValue:options[UIApplicationOpenURLOptionsAnnotationKey] forKey:@"annotation"];
+    }
+
+    // all plugins will get the notification, and their handlers will be called
+    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:CDVPluginHandleOpenURLNotification object:url]];
+    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:CDVPluginHandleOpenURLWithAppSourceAndAnnotationNotification object:openURLData]];
+
+    return YES;
 }
 
 @end
